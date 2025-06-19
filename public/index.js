b719eb90ee8f29e33f90d9a088ae877613d5fa3e
nadiachaja <nadiagaja13@gmail.com>
2025-06-19 12:24:39 +0200

    comments js

--- public/index.js
+++ public/index.js
@@ -1,30 +1,30 @@
-const inputSearchbar = document.getElementById('search');
-const allBooks = document.querySelectorAll('.books-container');
+const inputSearchbar = document.getElementById('search'); // selecteert de input met id search
+const allBooks = document.querySelectorAll('.books-container'); // selecteert de boeken met class books-container
 
-inputSearchbar.addEventListener('input', () => {
-  const filter = inputSearchbar.value.toLowerCase();
-  allBooks.forEach(book => {
-    const title = book.querySelector('.text-info.titel').textContent.toLowerCase();
-    if (title.includes(filter)) {
-      book.style.display = '';
+inputSearchbar.addEventListener('input', () => { //luistert naar de input als je iets typt
+  const filter = inputSearchbar.value.toLowerCase(); //maakt van alle tekst die je invoert kleine letters
+  allBooks.forEach(book => { //loop door alle boeken 
+    const title = book.querySelector('.text-info.titel').textContent.toLowerCase(); //pakt de titel met de class text-info.titel in de boeken en wordt ook in kleine letters omgezet
+    if (title.includes(filter)) { //als de zoekterm overeen komt met de titel laat het boek zien anders verberg het boek
+      book.style.display = ''; //laat zien
     } else {
-      book.style.display = 'none';
+      book.style.display = 'none'; //verberg
     }
   });
 });
 
 
-window.addEventListener("mousemove", (e) => {
-  gsap.to(".cursor", {
-    x: e.clientX,
+window.addEventListener("mousemove", (e) => {  //luister naar de muisbeweging over de hele pagina en wordt uitgevoerd al je muisbeweegt
+  gsap.to(".cursor", { // wordt gekeken waar de muis is zodat de .cursor daar ook is 
+    x: e.clientX,  
     y: e.clientY
   });
-  if (e.target.nodeName == "A" || e.target.closest('a')) {
+  if (e.target.nodeName == "A" || e.target.closest('a')) { // als de muis over de een a gaat dan wordt de .cursor groter // check ook of de en ook een a in een ander element zit
     gsap.to(".cursor", {
       scale: 1
     });
   } else {
-    gsap.to(".cursor", {
+    gsap.to(".cursor", { // als de muis niet over een a gaat dan verdwijnt de .cursor 
       scale: 0
     });
   }
